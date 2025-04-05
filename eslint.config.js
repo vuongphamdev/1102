49f413e753ee9445d61619c11263a031b61bedf6
vuongphamdev <phamnhuvuong96@gmail.com>
2025-04-06 00:06:17 +0700

    updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,50 +1,27 @@
-import eslint from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tseslintParser from '@typescript-eslint/parser';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import pluginNext from '@next/eslint-plugin-next';
+import parser from '@typescript-eslint/parser';
 
 export default [
   {
-    ignores: ['node_modules/**', '.next/**', 'build/**', 'dist/**', 'public/**']
-  },
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    name: 'ESLint Config - nextjs',
+    ignores: ['node_modules/**', '.next/**', 'build/**', 'dist/**', 'public/**', 'eslint.config.js'],
     languageOptions: {
-      parser: tseslintParser,
+      parser,
       parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        },
         ecmaVersion: 'latest',
         sourceType: 'module',
-        globals: {
-          ...globals.browser,
-          ...autoImportGlobals.globals,
+        ecmaFeatures: {
+          jsx: true,
         },
-      }
+      },
     },
     plugins: {
-      '@typescript-eslint': tseslint,
-      'react': reactPlugin,
-      'react-hooks': reactHooksPlugin
+      '@next/next': pluginNext,
     },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     rules: {
-      ...eslint.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      ...reactPlugin.configs.recommended.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'react-hooks/exhaustive-deps': 'warn'
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs['core-web-vitals'].rules,
     },
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
-  }
-]; 
+  },
+];
